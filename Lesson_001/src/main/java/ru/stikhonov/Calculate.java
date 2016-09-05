9150f0eef2ed6c1c5f4273c11d02ee781f34e3a7
stikhonov <tikhonov.srg@gmail.com>
2016-09-05 19:38:13 +1100

    Initial commit

---
+++ Lesson_001/src/main/java/ru/stikhonov/Calculate.java
@@ -0,0 +1,14 @@
+package ru.stikhonov;
+
+/**
+ * Created by Sergey Tikhonov on 05.09.2016.
+ */
+public class Calculate {
+    /**
+     * @param args Чоза
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World .. Again");
+    }
+
+}
