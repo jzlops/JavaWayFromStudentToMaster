5d4a725e4d87f473bed263aa4bd251e8b44a3d96
stikhonov <tikhonov.srg@gmail.com>
2016-09-09 18:44:14 +1100

    d

--- Lesson_001/src/main/java/ru/stikhonov/term3/Run.java
+++ Lesson_001/src/main/java/ru/stikhonov/term3/Run.java
@@ -7,6 +7,6 @@ public class Run {
     public static void main(String[] args) {
         Triangle triangle = TriangleActions.initTriangle(1, 1, 3, 5, 6, 1);
         System.out.println("Triangle area = " + triangle.getArea());
-        System.out.println("Triangle max Leg = " + TriangleActions.getMaxTriangleLeg(triangle));
+        System.out.println("Triangle max leg length = " + TriangleActions.getMaxTriangleLeg(triangle));
     }
 }
