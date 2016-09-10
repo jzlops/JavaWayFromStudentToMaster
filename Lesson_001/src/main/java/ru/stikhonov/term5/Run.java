8cf62762cd281fa561c0d68a1f2d26d487920eb7
stikhonov <tikhonov.srg@gmail.com>
2016-09-10 22:04:10 +1100

    d

--- Lesson_001/src/main/java/ru/stikhonov/term5/Run.java
+++ Lesson_001/src/main/java/ru/stikhonov/term5/Run.java
@@ -7,6 +7,8 @@ public class Run {
     public static void main(String[] args) {
         int[] array1X = ArrayGenerator.generate1X(10);
         int[][] array2X = ArrayGenerator.generate2X(10);
+        ArrayActions.bubleSort(array1X);
+        array2X = ArrayActions.rotate(array2X);
 
     }
 }
