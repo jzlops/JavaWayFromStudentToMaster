9cd9c441c5da18f11172506c45a3a4e69954dd75
stikhonov <tikhonov.srg@gmail.com>
2016-09-13 16:45:59 +1100

    fck fix

--- Lesson_001/src/main/java/ru/stikhonov/term5/ArrayStringDuplicateTruncate.java
+++ Lesson_001/src/main/java/ru/stikhonov/term5/ArrayStringDuplicateTruncate.java
@@ -1,5 +1,7 @@
 package ru.stikhonov.term5;
 
+import java.util.Arrays;
+
 /**
  * Created by Sergey Tikhonov on 12.09.2016.
  */
@@ -7,31 +9,30 @@ public class ArrayStringDuplicateTruncate {
     /**
      * Метод удаляет дубликаты строк из массива String[]
      *
-     * @param strings вхоодной параметр - массив String[]
+     * @param strings входной параметр - массив String[]
      * @return возвращает ссылку на новый массив String[] без дубликатов
      */
+
     public String[] duplicateStringKill(String[] strings) {
-        int fillArrayCounter = 0;
-        int duplicateCount = 0;
-        final String EMPTY = "EMPTY";
-        String[] tempStrings;
-        for (int i = 0; i < strings.length; i++)
-            if (!(strings[i].equals(EMPTY)))
-                for (int j = i; j < strings.length - 1; j++)
-                    if (strings[i].equals(strings[j + 1])) {
-                        duplicateCount++;
-                        strings[j + 1] = EMPTY;
-                        j++;
-                    }
+        int fitCount = 0;
+        int lengthArray = strings.length - 1;
 
-        tempStrings = new String[strings.length - duplicateCount];
-
-        for (String s : strings)
-            if (!(s.equals(EMPTY))) {
-                tempStrings[fillArrayCounter] = s;
-                fillArrayCounter++;
+        for (int i = 0; i <= strings.length - 1; i++) {
+            for (int k = i + 1; k <= strings.length - 1; k++) {
+                if (strings[i] != null && strings[k] != null && strings[i].equals(strings[k])) {
+                    strings[k] = null;
+                    fitCount++;
+                }
             }
-        System.out.printf("Удаление дубликатов строкового массива выполнено %n%n");
-        return tempStrings;
+        }
+        for (int i = strings.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (strings[j] == null) {
+                    strings[j] = strings[j + 1];
+                    strings[j + 1] = null;
+                }
+            }
+        }
+        return Arrays.copyOf(strings, strings.length - fitCount);
     }
 }
