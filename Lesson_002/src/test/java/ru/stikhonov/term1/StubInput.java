aeef9e29023d3150d71b22df210fbcc8b800aa2a
stikhonov <tikhonov.srg@gmail.com>
2016-09-26 22:48:17 +1100

    Testin Show and Delete

--- Lesson_002/src/test/java/ru/stikhonov/term1/StubInput.java
+++ Lesson_002/src/test/java/ru/stikhonov/term1/StubInput.java
@@ -1,5 +1,6 @@
 package ru.stikhonov.term1;
 
+import org.junit.Before;
 import ru.stikhonov.term1.Tracker;
 
 import java.util.Date;
@@ -14,12 +15,36 @@ import static org.mockito.Mockito.spy;
 /**
  * @author Sergey Tikhonov
  */
+
 public class StubInput {
+
     @Test
-    public void addItemTest() {
-        Input testInput = new TestInput(new String[]{"1", "Jack", "Adding Item", "It is working", "0",});
+    public void testingAddItems() {
+        Input testInput = new TestInput(new String[]{"1", "Jack", "Adding Item", "It is working","1"
+                , "Serg", "Adding Item again", "It is working again", "0"});
         Tracker tracker = new Tracker(1);
         ConsoleMainMenu consoleMainMenu = new ConsoleMainMenu(tracker, testInput);
         consoleMainMenu.start();
     }
+
+    @Test
+    public void testingAddItemsAndShowItems() {
+        Input testInput = new TestInput(new String[]{"1", "Jack", "Adding Item", "It is working","1"
+                , "Serg", "Adding Item again", "It is working again", "0"});
+        Tracker tracker = new Tracker(1);
+        ConsoleMainMenu consoleMainMenu = new ConsoleMainMenu(tracker, testInput);
+        consoleMainMenu.start();
+    }
+
+    @Test
+    public void testingDeleteItem() {
+        Input testInput = new TestInput(new String[]{"3", "RQS888888", "0"});
+        Tracker tracker = new Tracker(1);
+        Item item = new Item("Lena", "Deleting Item", new Date(), "Ololo");
+        tracker.addItem(item);
+        item.setItemID("RQS888888");
+        ConsoleMainMenu consoleMainMenu = new ConsoleMainMenu(tracker, testInput);
+        consoleMainMenu.start();
+    }
+
 }
